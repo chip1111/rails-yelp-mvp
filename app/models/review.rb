35dcc31ba732cc7ee2477fbd4a25cc014146a712
Jamie Walden <jamiewalden@gmail.com>
2017-05-24 15:06:29 +0100

    raking green commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ApplicationRecord
 
   validates :restaurant_id, presence: true
   validates :content, presence: true
+  validates_numericality_of :rating, :on => :create
   validates :rating, inclusion: { in: (0..5).to_a, allow_nil: false }
 
 end
